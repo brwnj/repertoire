3cb02dbc6a97613dfd08534a5e3e1215aa8baa50
brwnj <brwnjm@gmail.com>
2013-04-08 11:01:02 -0600

    meta from joined reads

---
+++ repertoire/joined_reads_meta.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Parse the output of SeqPrep joined reads when using trim_adapter.py's output.
+"""
+from toolshed import nopen
+from parsers import read_fastx
+from collections import Counter
+
+def main(args):
+    for fastq in args.fastq:
+        print fastq
+        meta = Counter()
+        with nopen(fastq) as fh:
+            for name, seq, qual in read_fastx(fh):
+                name, cregion, fwork = name.split()[0].split(":")
+                meta.update(["%s:%s" % (cregion, fwork)])
+        for combination, count in meta.iteritems():
+            print "%s\t%d" % (combination, count)
+
+if __name__ == '__main__':
+    import argparse
+    p = argparse.ArgumentParser(description=__doc__,
+            formatter_class=argparse.RawDescriptionHelpFormatter)
+    p.add_argument('fastq', nargs="+", help="SeqPrep joined reads.")
+    main(p.parse_args())
