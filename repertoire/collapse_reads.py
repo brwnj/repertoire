71ec3e1d4e2f9fb48b7afbbaaf5962a62d9ead3d
brwnj <brwnjm@gmail.com>
2013-04-18 11:20:46 -0600

    edit distance rather than pairwise2

--- repertoire/collapse_reads.py
+++ repertoire/collapse_reads.py
@@ -4,9 +4,9 @@
 Find unique reads by primer among all joined reads.
 """
 import sys
+import editdist as ed
 from toolshed import nopen
 from parsers import read_fastx
-from Bio import pairwise2
 from collections import OrderedDict
 
 def fastq_to_dict(fastq):
@@ -17,38 +17,39 @@ def fastq_to_dict(fastq):
             d[name] = {'seq':seq,'qual':qual}
     return d
 
+def distance(a, b):
+    """Calculate Levenshtein distance accounting for length differences between
+    the two strings. Returns int.
+    >>> distance('CATGGGTGGTTCAGTGGTAGAATTCTCGCCTGCC', 'GTGCTGTAGGCATT')
+    2
+    """
+    return ed.distance(a, b) - abs(len(a) - len(b))
+
 def main(args):
     fd = fastq_to_dict(args.fastq)
-    # convert to ordered dictionary
-    fd = OrderedDict(sorted(fd.items(), key=lambda (k, v): len(v['seq'])))
-    seen = {}
-    for i, (name, query) in enumerate(fd.iteritems(), start=1):
-        if i % 1000 == 0:
+    # longest sequence first
+    fd = OrderedDict(sorted(fd.items(), key=lambda (k, v): len(v['seq']), reverse=True))
+    seen = set()
+    ignore = set()
+    for i, (t_name, target) in enumerate(fd.iteritems(), start=1):
+        # skip reads that have already been determined as subsequences
+        if t_name in ignore: continue
+        seen.add(t_name)
+        if i % 10 == 0:
             print >> sys.stderr, ">> processed %d reads..." % i
-        subseq = False
-        q_id, q_cregion, q_fwork = name.split(":")
-        expected_score = len(query['seq']) - args.mismatches
-        # maps onto same length or longer seqs
-        for t_name, target in fd.iteritems():
-            if t_name == name: continue
-            # skipping reads we've already mapped
-            if seen.has_key(t_name): continue
-            t_id, t_cregion, t_fwork = t_name.split(":")
+        t_id, t_cregion, t_fwork = t_name.split(":")
+        for q_name, query in fd.iteritems():
+            # seen - tested in first loop
+            # ignore - already found to be subsequence of a target
+            if q_name in seen or q_name in ignore: continue
+            q_id, q_cregion, q_fwork = q_name.split(":")
             # only attempt to collapse things of the same c-region and framework
-            if q_cregion != t_cregion and q_fwork != t_fwork: continue
-            # locally align using smith-waterman
-            matches = pairwise2.align.localms(target['seq'], query['seq'], 1, -1, -1, -1)
-            try:
-                high_score = matches[0][2]
-            except IndexError:
-                continue
-            if high_score == expected_score:
-                subseq = True
-                break
-        if not subseq:
-            # print fastq record
-            print "@%s\n%s\n+\n%s" % (name, query['seq'], query['qual'])
-        seen[name] = ""
+            if t_cregion != q_cregion and t_fwork != q_fwork: continue
+            # compare the sequences
+            if distance(target['seq'], query['seq']) < args.mismatches:
+                # mark subsequences to be ignored
+                ignore.add(q_name)
+        print "@%s\n%s\n+\n%s" % (t_name, target['seq'], target['qual'])
 
 if __name__ == '__main__':
     import argparse
@@ -56,5 +57,5 @@ if __name__ == '__main__':
             formatter_class=argparse.RawDescriptionHelpFormatter)
     p.add_argument('fastq', help="reads to collapse to unique")
     p.add_argument('-m', '--mismatches', type=int, default=0,
-            help="mismatches to allow during mapping [ %(default)s ]")
+            help="mismatches to allow during mapping [%(default)s]")
     main(p.parse_args())
